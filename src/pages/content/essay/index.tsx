61d122afc7e6cf561b46ac5fa4bb2bc25da244b3
panjinshan <pjspan@foxmail.com>
2023-05-23 18:04:01 +0800

    @luminous1011 优化

--- src/pages/content/essay/index.tsx
+++ src/pages/content/essay/index.tsx
@@ -1,5 +1,6 @@
 import { DownOutlined, PlusOutlined, UpOutlined } from '@ant-design/icons';
-import { Button, message, Input, Drawer, Card, Col, Form, Row, Table } from 'antd';
+import { Button, message, Input, DatePicker, Card, Col, Form, Row, Select } from 'antd';
+import type { DatePickerProps } from 'antd';
 import React, { useState, useEffect, useRef } from 'react';
 import { PageContainer, FooterToolbar } from '@ant-design/pro-layout';
 import type { ProColumns, ActionType } from '@ant-design/pro-table';
@@ -9,13 +10,11 @@ import type { ProDescriptionsItemProps } from '@ant-design/pro-descriptions';
 import ProDescriptions from '@ant-design/pro-descriptions';
 import type { FormValueType } from './components/UpdateForm';
 import UpdateForm from './components/UpdateForm';
-import TableWrapper from '@/components/TableWrapper'
+import TableWrapper from '@/components/TableWrapper';
 import type { ColumnsType, ColumnType } from 'antd/es/table';
 import { rule, addRule, updateRule, removeRule, getList } from './service';
 import type { SearchBtnCrtlsItem, IEssay, TableListItem, TableListPagination } from './data';
-import './index.less'
-
-
+import './index.less';
 
 const columns: ColumnType<object>[] = [
   {
@@ -43,30 +42,37 @@ const columns: ColumnType<object>[] = [
     title: '更新时间',
     dataIndex: 'updateTime',
   },
+  {
+    title: '操作',
+    dataIndex: 'infoId',
+    render: (_, record) => [<a onClick={() => {}}>查看</a>, <a>编辑</a>, <a>删除</a>],
+  },
 ];
-
+const onChange: DatePickerProps['onChange'] = (date, dateString) => {
+  console.log(date, dateString);
+};
 const getData = async () => {
-  const res = await getList({ page: 1, pageSize: 10 })
-  const { list } = res.data
-  return list
-}
+  const res = await getList({ page: 1, pageSize: 10 });
+  const { list } = res.data;
+  return list;
+};
 
 const TableList: React.FC = () => {
-  const [visibleAll, setVisbleAll] = useState(false)
-  const [data, setData] = useState([] as IEssay[])
+  const [visibleAll, setVisbleAll] = useState(false);
+  const [data, setData] = useState([] as IEssay[]);
   const [form] = Form.useForm();
-  const loading = false
+  const loading = false;
   const formStyle = {
     maxWidth: 'none',
     padding: 24,
   };
   useEffect(() => {
-    getData().then(res => setData(res))
+    getData().then((res) => setData(res));
   }, []);
 
   const handleVisibleAll = () => {
-    setVisbleAll(!visibleAll)
-  }
+    setVisbleAll(!visibleAll);
+  };
   // 默认Buttons []
   const searchBtnCrtls: SearchBtnCrtlsItem[] = [
     {
@@ -84,86 +90,112 @@ const TableList: React.FC = () => {
       content: '更多',
       type: 'link',
       visibleSwitch: true,
-      event: handleVisibleAll
+      event: handleVisibleAll,
     },
-  ]
+  ];
   // 搜索表单项 []
-  const searchFormCloums: {
-    name: string,
-    label: string,
-    placeholder?: string | undefined
+  const searchFormColumns: {
+    name: string;
+    label: string;
+    placeholder?: string | undefined;
+    type: 'input' | 'select' | 'date';
   }[] = [
-      {
-        name: 'text',
-        label: '内容',
-      },
-      {
-        name: 'source',
-        label: '来源',
-      },
-      {
-        name: 'createTime',
-        label: '创建时间',
-      },
-      {
-        name: 'updateTime',
-        label: '更新时间',
-      },
-    ]
-  if (searchFormCloums.length <= 3) {
-    searchBtnCrtls.pop()
+    {
+      name: 'text',
+      label: '内容',
+      type: 'input',
+    },
+    {
+      name: 'source',
+      label: '来源',
+      type: 'select',
+    },
+    {
+      name: 'createTime',
+      label: '创建时间',
+      type: 'date',
+    },
+    {
+      name: 'updateTime',
+      label: '更新时间',
+      type: 'date',
+    },
+  ];
+  if (searchFormColumns.length <= 3) {
+    searchBtnCrtls.pop();
   }
+
+  const renderSearchFromColumns = (type: string) => {
+    switch (type) {
+      case 'input':
+        return <Input placeholder={'请输入'} />;
+      case 'select':
+        return (
+          <Select
+            defaultValue="lucy"
+            options={[
+              { value: 'jack', label: 'Jack' },
+              { value: 'lucy', label: 'Lucy' },
+              { value: 'Yiminghe', label: 'yiminghe' },
+              { value: 'disabled', label: 'Disabled' },
+            ]}
+          />
+        );
+      case 'date':
+        return <DatePicker onChange={onChange} />;
+    }
+  };
   /**
    * 渲染搜索表单
-   * @returns 
+   * @returns
    */
   const getTableSearchHeaderColums = () => {
-    const children = []
-    for (let i = 0; i < searchFormCloums.length; i++) {
+    const children = [];
+    for (let i = 0; i < searchFormColumns.length; i++) {
       children.push(
-        i < 3 || visibleAll ?
+        i < 3 || visibleAll ? (
           <Col span={8} key={i}>
-            <Form.Item
-              name={searchFormCloums[i].name}
-              label={searchFormCloums[i].label}
-            >
-              <Input placeholder={searchFormCloums[i]?.placeholder || '请输入'} />
+            <Form.Item name={searchFormColumns[i].name} label={searchFormColumns[i].label}>
+              {renderSearchFromColumns(searchFormColumns[i].type)}
+              {/* <Input placeholder={searchFormColumns[i]?.placeholder || '请输入'} /> */}
             </Form.Item>
           </Col>
-          : null
-      )
+        ) : null,
+      );
     }
-    return children
-  }
+    return children;
+  };
   /**
    * 合并 搜索表单 Buttons 布局
-   * @returns 
+   * @returns
    */
   const TableSearchHeader = () => {
     return (
       <Card>
-        <Form form={form} name="tableSearchForm" style={formStyle} >
-          <Row gutter={24} >{getTableSearchHeaderColums()}</Row>
-          <Row className='tableSearchForm-crtls' >
-            {
-              searchBtnCrtls.map((item) => (
-                item.loading ?
-                  <Button type={item.type} loading={loading} onClick={item.event}>
-                    {item.content}
-                  </Button>
-                  :
-                  <Button type={item.type} className={item.visibleSwitch ? 'visible-switch-btn' : ''} onClick={item.event}>
-                    {item.content} {
-                      item.visibleSwitch ? visibleAll ? <UpOutlined /> : <DownOutlined /> : null
-                    }
-                  </Button>
-              ))
-            }
+        <Form form={form} name="tableSearchForm" style={formStyle}>
+          <Row gutter={24}>{getTableSearchHeaderColums()}</Row>
+          <Row className="tableSearchForm-crtls">
+            {searchBtnCrtls.map((item) =>
+              item.loading ? (
+                <Button type={item.type} loading={loading} onClick={item.event}>
+                  {item.content}
+                </Button>
+              ) : (
+                <Button
+                  type={item.type}
+                  className={item.visibleSwitch ? 'visible-switch-btn' : ''}
+                  onClick={item.event}
+                >
+                  {item.content}{' '}
+                  {item.visibleSwitch ? visibleAll ? <UpOutlined /> : <DownOutlined /> : null}
+                </Button>
+              ),
+            )}
           </Row>
         </Form>
-      </Card >
-    )
-  }
+      </Card>
+    );
+  };
 
   return (
     <PageContainer>
